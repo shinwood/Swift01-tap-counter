a2d25586b358673633391b6e2810e405828868ca
shinwood <shinwood@gmail.com>
2016-05-01 11:21:36 +0800

    2nd commit

--- README.md
+++ README.md
@@ -3,3 +3,18 @@ My Swift-Learning Projects.
 
 *Inspired by http://samvlu.com/*
 
+
+
+- 函数及常量
+
+- UILabel 和 UIButton
+
+- 使用 Ctrl + 拖动 创建 Autolayout 规则
+
+- 使用 Ctrl + 拖动 创建 IBOutlet 和 IBAction
+
+- Unwrap an Optional
+
+- NSNumberformatter
+
+  ​
--- Swfit01-tap-counter/Swfit01-tap-counter/ViewController.swift
+++ Swfit01-tap-counter/Swfit01-tap-counter/ViewController.swift
@@ -10,16 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var counter: UILabel!
+    
+    let formatter = NSNumberFormatter()
+    var theCounter: Int = 0
+    @IBAction func addCounter(sender: UIButton) {
+
+        theCounter = formatter.numberFromString(counter.text!)!.integerValue + 1
+        counter.text = "\(theCounter)"
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func clearCounter(sender: UIButton) {
+        counter.text = "0"
+        theCounter = 0
     }
-
-
+    
 }
 

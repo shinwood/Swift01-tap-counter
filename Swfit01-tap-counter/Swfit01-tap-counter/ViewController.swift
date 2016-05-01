43e61e9141ae16c729d0619112760b0d85a84807
shinwood <shinwood@gmail.com>
2016-05-01 11:15:23 +0800

    completed

---
+++ Swfit01-tap-counter/Swfit01-tap-counter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swfit01-tap-counter
+//
+//  Created by shinwood on 5/1/16.
+//  Copyright © 2016 guox.in. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
